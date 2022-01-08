7e90e3e3a1553d541dfd24fffa05497c1d775f8b
Mushtaba <MushtabaA1234@gmail.com>
2022-01-08 16:49:23 -0700

    completed home page, and added public folder for styles

--- server/server.js
+++ server/server.js
@@ -10,3 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
 });
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
