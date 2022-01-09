7c52029baeee9fedd15988012a4d6d2aaad43f18
Mushtaba <MushtabaA1234@gmail.com>
2022-01-08 21:01:29 -0700

    created login/signup pages, inital setup with database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+require('dotenv').config();
 
 const app = express();
 
@@ -7,10 +9,20 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
 });
 
 app.get("/", function(req, res) {
     res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/signup", function(req, res) {
+    res.render("signup");
 });
